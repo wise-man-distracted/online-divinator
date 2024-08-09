b1c021b79bfca25489d28becce3632c1105d6505
wise-man-distracted <magewelle@live.com>
2024-08-09 20:20:22 -0300

    init tarot boilerplate

---
+++ .svelte-kit/generated/client/nodes/3.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/4CardSpread/+page.svelte";
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+
+<h1>Welcome to {name}</h1>
+<p>{deck}</p>
+<p></p>
+<p>{JSON.stringify(images.cards, null, 2)}</p>
+<!-- <img alt="Card" src="./tarot-json/cards/{images.cards[1].img}"> -->
+
+<script lang="ts">
+    
+    import images from '../../static/tarot-json/tarot-images.json';
+    // let imagesJSON = JSON.parse(images);
+    console.log(images.cards[1]);
+	let name : string = 'Svelte';
+    let deck : number[] = [0, 1, 5, 7, 78];
+
+
+</script>
